330656b9873900890a8b74760bce2a9e41c9fe94
Isham Araia <ishamaraia@gmail.com>
2020-02-26 16:47:41 +0000

    Refactor and optimization

--- clumsy-factorial/solution.py
+++ clumsy-factorial/solution.py
@@ -1,23 +1,17 @@
+from operator import add, sub, mul, truediv 
 class Solution:
+
     def clumsy(self, N: int) -> int:
-
-        N = N + 1
-        value = N - 1
+        N, value = N + 1, N - 1 
         store = []
-        ops = {
-            1: lambda x,y: x * y,
-            2: lambda x,y: x / y,
-            3: lambda x,y: x + y,
-            0: lambda x,y: x - y, 
-        }
-
+        ops = [sub, mul, truediv, add]
+        
+        # Normalize list to -> sum/
         for n in range(1,N):
-
             num = (N - n)
             next_num = (num - 1)
             if num > 1:
-                #normalize list
-                if (n % 4) in [0,3]:
+                if (n % 4) in [0, 3]:
                     store.append(value)
                     value = next_num
                 else:
@@ -25,14 +19,14 @@ class Solution:
             else:
                 store.append(value)
 
-        # SUM -> this can be converted to a list comprehension
-
+        # Calculate value
         value = store[0]
-        for x in range(len(store)):
-            if x < (len(store) - 1):
-                if (x % 2) == 0:
-                    value = int(value + store[x + 1])
-                else:
-                    value = int(value - store[x + 1])
-
+        for x in range(len(store) - 1):
+            if (x % 2) == 0:
+                value = value + store[x + 1]
+            else:
+                value = value - store[x + 1]
         return value
+
+test = Solution()
+print(test.clumsy(10))
--- longest-substring-without-repeating-characters/solution.py
+++ longest-substring-without-repeating-characters/solution.py
@@ -1,20 +1,18 @@
-#!/bin/python
-# Sliding window
+class Solution:
+    def findsubstrlen(self, s):
+        candidate   = str()
+        store       = {}
+        result      = str()
 
-def findsubstrlen(s):
-    candidate   = str()
-    store       = {}
-    result      = str()
+        for n in range(len(s)):
+            if s[n] in candidate:
+                candidate = s[int(store[s[n]] + 1):n]
+            store[s[n]] = n
+            candidate += s[n]
+            if len(candidate) > len(result):
+                result = candidate
 
-    for n in range(len(s)):
-        if s[n] in candidate:
-            candidate = s[int(store[s[n]] + 1):n]
-        store[s[n]] = n
-        candidate += s[n]
-        if len(candidate) > len(result):
-            result = candidate
+        return len(result)
 
-    return len(result)
-
-inputstr = "nfpdmpi"
-print(findsubstrlen(inputstr))
+test = Solution()
+print(test.findsubstrlen("nfpdmpi"))
